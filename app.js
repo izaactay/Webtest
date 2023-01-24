9179a6bb2ed428799c7e30fa8447c4fa2e11bd50
izaactay <105307340+izaactay@users.noreply.github.com>
2023-01-25 07:20:58 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const codeCardTemplate = document.querySelector("[data-code-template")
+const codeCardContainer = document.querySelector("[data-code-template")
+
+const searchInput = document.querySelector("[data-search]")
+
+searchInput.addEventListener("input", e =>{
+    const value = e.target.value
+    console.log(value)
+})
+
+fetch('./codes.json')
+    .then(res => res.json())
+    .then(data => {
+        codes = data.map( code => {
+            const card = codeCardTemplate.content.cloneNode(true).children[0]
+            const codename = card.querySelector("[data-code-name]")
+            const description = card.querySelector("[data-description]")
+            codename.textContent = code.code
+            description.textContent = code.description
+            codeCardContainer.append(card)
+        })
+    })
