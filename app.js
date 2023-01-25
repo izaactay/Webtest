09fb2d3dc4d61076bafc2782a66b2ec63e33fbec
izaactay <105307340+izaactay@users.noreply.github.com>
2023-01-25 08:10:12 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,28 @@
-const codeCardTemplate = document.querySelector("[data-code-template")
-const codeCardContainer = document.querySelector("[data-code-template")
-
+const codeCardTemplate = document.querySelector("[data-code-template]")
+const codeCardContainer = document.querySelector("[data-code-cards-container]")
 const searchInput = document.querySelector("[data-search]")
 
-searchInput.addEventListener("input", e =>{
-    const value = e.target.value
-    console.log(value)
+let codes = []
+
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  codes.forEach(code => {
+    const isVisible = code.Code.toLowerCase().includes(value)
+    code.element.classList.toggle("hide", !isVisible)
+  })
 })
 
-fetch('codes.json')
-    .then(res => res.json())
-    .then(data => {
-        codes = data.map( code => {
-            const card = codeCardTemplate.content.cloneNode(true).children[0]
-            const codename = card.querySelector("[data-code-name]")
-            const description = card.querySelector("[data-description]")
-            codename.textContent = code.code
-            description.textContent = code.description
-            codeCardContainer.append(card)
-        })
+fetch("https://izaactay.github.io/codes.json")
+  .then(res => res.json())
+  .then(data => {
+    codes = data.map(code => {
+      const card = codeCardTemplate.content.cloneNode(true).children[0]
+      const header = card.querySelector("[data-header]")
+      const body = card.querySelector("[data-body]")
+      header.textContent = code.Code
+      body.textContent = code.Description
+      codeCardContainer.append(card)
+      return { Code: code.Code, Description: code.Description, element: card }
+      
     })
+  })
